38831e1c0538719e4da47783abcc4cf43b21a1c5
Keito654 <keito19981129@gmail.com>
2023-02-07 01:38:09 +0900

    actionを追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-next",
+    "@storybook/addon-actions"
     {
       name: "@storybook/addon-postcss",
       options: {
